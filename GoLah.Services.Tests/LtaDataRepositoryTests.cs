684a7025a4609d55e1c083862b5fa8e513afcee1
wu bin <danielwub@hotmail.com>
2017-01-09 17:46:51 +0800

    Add LtaDataRepository as data service.

---
+++ GoLah.Services.Tests/LtaDataRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System.Threading.Tasks;
+
+namespace GoLah.Services.Tests
+{
+    [TestClass]
+    public class LtaDataRepositoryTests
+    {
+        [TestMethod]
+        public async Task GetBusStopssAsync()
+        {
+            var repository = new LtaDataRepository();
+            var result = await repository.GetBusStopsAsync();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetBusServicesAsync()
+        {
+            var repository = new LtaDataRepository();
+            var result = await repository.GetBusServicesAsync();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public async Task GetNextBusAsync()
+        {
+            var repository = new LtaDataRepository();
+            var stopId = "83319";
+            var result = await repository.GetNextBusAsync(stopId);
+            Assert.IsNotNull(result);
+        }
+
+
+    }
+}
