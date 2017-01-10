221af8eb235113d7378ef41235c99666b2910dec
wu bin <danielwubin@gmail.com>
2017-01-10 17:12:11 +0800

    Minor enhancement & bug fix.

--- GoLah.Services.Tests/LtaDataRepositoryTests.cs
+++ GoLah.Services.Tests/LtaDataRepositoryTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using GoLah.Model;
 
 namespace GoLah.Services.Tests
 {
@@ -11,16 +13,16 @@ namespace GoLah.Services.Tests
         public async Task GetBusStopssAsync()
         {
             var repository = new LtaDataRepository();
-            var result = await repository.GetBusStopsAsync();
-            Assert.IsNotNull(result);
+            var result = new List<BusStop>(await repository.GetBusStopsAsync());
+            Assert.AreEqual(50, result.Count);
         }
 
         [TestMethod]
         public async Task GetBusServicesAsync()
         {
             var repository = new LtaDataRepository();
-            var result = await repository.GetBusServicesAsync();
-            Assert.IsNotNull(result);
+            var result = new List<BusService>(await repository.GetBusServicesAsync());
+            Assert.AreEqual(50, result.Count);
         }
 
         [TestMethod]
@@ -28,8 +30,11 @@ namespace GoLah.Services.Tests
         {
             var repository = new LtaDataRepository();
             var stopId = "83319";
-            var result = await repository.GetNextBusAsync(stopId);
-            Assert.IsNotNull(result);
+            var result = new List<ArrivalBusService>(await repository.GetNextBusAsync(stopId));
+            Assert.IsTrue(result.Count == 3);
+            Assert.AreEqual("13", result[0].ServiceNo);
+            Assert.AreEqual("55", result[1].ServiceNo);
+            Assert.AreEqual("966", result[2].ServiceNo);
         }
 
 
